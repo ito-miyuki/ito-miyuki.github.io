79d0efe8c8962c3a1f74e3587ba5fb78fbbf4329
Miyuki Ito <miyuki.ito@slurp.coffee>
2025-01-08 22:05:35 +0200

    Added correct pictures

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,31 +1,35 @@
 import React from "react"
 import "./Projects.css"
-import project1 from "../assets/miyuki_ito.jpg"
-import project2 from "../assets/miyuki_ito.jpg"
-import project3 from "../assets/miyuki_ito.jpg"
-import project4 from "../assets/miyuki_ito.jpg"
+import project1 from "../assets/C_Language_Logo.jpg"
+import project2 from "../assets/cub3d.jpeg"
+import project3 from "../assets/bash.jpg"
+import project4 from "../assets/solong.jpeg"
 
 const Projects: React.FC = () => {
     const projects = [
         {
-            title: "example1",
-            description: "add description here1",
-            imageUrl: project1
+            title: "Capybara maze",
+            description: "2D capybara game in C++ with SFML",
+            imageUrl: project4,
+            link: "https://github.com/ito-miyuki/solong-cpp"
         },
         {
-            title: "example2",
-            description: "add description here1",
-            imageUrl: project2
+            title: "minishell",
+            description: "create shell in C",
+            imageUrl: project3,
+            link: "https://github.com/ito-miyuki/minishell"
         },
         {
-            title: "example3",
-            description: "add description here1",
-            imageUrl: project3
+            title: "cub3D",
+            description: "3D game built in C with raycasting",
+            imageUrl: project2,
+            link: "https://github.com/ito-miyuki/cub3d"
         },
         {
-            title: "example4",
-            description: "add description here1",
-            imageUrl: project4
+            title: "Libft",
+            description: "The imitation of functions in the C library",
+            imageUrl: project1,
+            link: "https://github.com/ito-miyuki/libft"
         },
     ]
     return (
@@ -34,11 +38,14 @@ const Projects: React.FC = () => {
                 <h2>My Projects</h2>
                 <div className="project-grid">
                     {projects.map((project, index) => (
-                        <div key={index} className="project-card">
-                            <img src={project.imageUrl} alt={project.title} />
-                            <h3>{project.title}</h3>
-                            <p>{project.description}</p>
-                        </div>
+                        <a href={project.link} key={index} className="project-card" target="_blank" rel="noopener noreferrer">
+                            <div>
+                                <img src={project.imageUrl} alt={project.title} />
+                                <h3>{project.title}</h3>
+                                <p>{project.description}</p>
+                            </div>
+                        </a>
+
                     ))}
                 </div>
             </div>
