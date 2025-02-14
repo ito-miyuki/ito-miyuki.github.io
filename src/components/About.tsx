0deee9bca58fc84270cbefdae8367ae19eb5e2e9
Miyuki Ito <miyuki.ito@slurp.coffee>
2025-02-14 12:54:22 +0200

    added underlines using Rough notation

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { annotate, annotationGroup } from "rough-notation";
+
 import "./About.css";
 import miyuki from "../assets/miyuki-no-bg.png";
-import { motion } from "framer-motion";
 import Links from "./Links";
 
 const About: React.FC = () => {
+
+    const refs = useRef<(HTMLSpanElement | null)[]>([]);
+
+    useEffect(() => {
+        const annotations = refs.current.map((el) => {
+            if (!el) return null;
+            return annotate(el, {
+                type: "underline",
+                color: "pink",
+                animationDuration: 1000,
+            });
+        }).filter(Boolean); // `null` を削除
+
+        const group = annotationGroup(annotations as any);
+        group.show(); // 一括でアニメーションを適用
+    }, []);
+
     return (
         <section className="about">
             <div className="about-content">
                 <div className="about-text">
-                    <motion.h2
-                    initial={{ scaleX: 0}}
-                    animate={{ scaleX: 1}}
-                    transition={{ duration: 1, ease: "easeInOut" }}
-                    >
+                    <h2>
                         <span className="thinner-text">Hello I'm</span> Miyuki<br/>Software <span className="outline-text">Developer</span><br/><span className="thinner-text">Based in </span>Helsinki
-                    </motion.h2>
+                    </h2>
                     <p>
-                        Software engineering student at Hive Helsinki, always eager to learn and grow. Passionate about problem-solving and creating user-friendly solutions, with a background in growth hacking and a curious mindset.
+                        Software engineering student at <span ref={(el) => (refs.current[0] = el)}>Hive Helsinki</span>, always eager to learn and grow. Passionate about <span ref={(el) => (refs.current[1] = el)}>problem-solving</span> and creating user-friendly solutions, with a background in growth hacking and a <span ref={(el) => (refs.current[2] = el)}>curious mindset</span>.
                     </p>
                     <p>
                         When I'm not coding, you can find me playing a guitar or brewing coffee.
@@ -30,7 +44,6 @@ const About: React.FC = () => {
             </div>
         </section>
     )
-
 }
 
 export default About;
